54995ed841134409f90d7d31339a46803f20d178
smot <smot@itu.dk>
2018-05-05 19:02:52 +0200

    Collision Particle

---
+++ Assets/Stinus/CollisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+
+    public ParticleSystem Particle;
+    public Transform ThisTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 parPosition = (ThisTransform.position + collision.transform.position) / 2;
+        //parPosition.z = -3;
+        Quaternion parRotation = Quaternion.LookRotation(ThisTransform.position - collision.transform.position);
+        Instantiate(Particle, parPosition, parRotation);
+    }
+}
---
+++ Assets/Stinus/ParticleRemover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRemover : MonoBehaviour
+{
+
+    public float RemovalDelay = 5;
+
+	// Use this for initialization
+	void Start () {
+	    Destroy(gameObject, RemovalDelay);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
