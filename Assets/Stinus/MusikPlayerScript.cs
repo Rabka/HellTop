dcc5f647c15579312ed0b16565d0c11384cb4189
smot <smot@itu.dk>
2018-05-06 10:04:07 +0200

    Menu and musik

---
+++ Assets/Stinus/MusikPlayerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusikPlayerScript : MonoBehaviour
+{
+
+    public List<AudioClip> MusikNumbers;
+    public AudioSource MusikSource;
+    public bool Mute = false;
+    private bool isMutet = false;
+
+    [Range(0f, 1f)]
+    public float Volume = 1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    MusikSource.clip = GetRandomMusik();
+	    MusikSource.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (!isMutet)
+	    {
+	        if (Mute)
+	        {
+	            MuteFlip();
+                return;
+	        }
+
+	        MusikSource.volume = Volume;
+            if (!MusikSource.isPlaying)
+	        {
+	            MusikSource.clip = GetRandomMusik();
+	            MusikSource.Play();
+	        }
+	    }
+	}
+
+    public bool MuteFlip()
+    {
+        Mute = !Mute;
+        MusikSource.mute = Mute;
+        isMutet = Mute;
+        return Mute;
+    }
+
+    AudioClip GetRandomMusik()
+    {
+        return MusikNumbers[(int) Mathf.Floor(Random.Range(0, MusikNumbers.Count))];
+    }
+}
---
+++ Assets/Stinus/PauseMenu.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = true;
+    public GameObject PauseMenuUI;
+    public MusikPlayerScript MusikPlayer;
+    public float MusikMax = .6f;
+    public float MusikMin = .3f;
+    public Text MuteText;
+    public RectTransform Cursor;
+    public float CursorMove = 40f;
+
+    private int _button = 0;
+    private float _lastButton = 0;
+    private float _waitButton = 0.2f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("AllPause") || Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        if (GameIsPaused)
+	        {
+	            Resume();
+	        }
+	        else
+	        {
+	            PauseMenuUI.SetActive(true);
+	            MusikPlayer.Volume = MusikMin;
+                //Time.timeScale = 0f;
+	            GameIsPaused = true;
+	        }
+	    }
+
+	    if ((Input.GetButtonDown("AllA") || Input.GetKeyDown(KeyCode.Space)) && Time.time > _lastButton + _waitButton)
+	    {
+	        _lastButton = Time.time;
+	        if (_button == 0)
+                Resume();
+	        if (_button == 1)
+	            Mute();
+	        if (_button == 2)
+	            Restart();
+        }
+
+	    if ((Input.GetAxis("AllVertical") < -0.8 || Input.GetKeyDown(KeyCode.UpArrow)) &&
+	        Time.time > _lastButton + _waitButton)
+	    {
+	        _lastButton = Time.time;
+	        if (_button > 0)
+	        {
+	            _button--;
+	            Cursor.position += new Vector3(0f, CursorMove);
+
+	        }
+	    }
+
+	    if ((Input.GetAxis("AllVertical") > 0.8 || Input.GetKeyDown(KeyCode.DownArrow)) &&
+	        Time.time > _lastButton + _waitButton)
+	    {
+	        _lastButton = Time.time;
+	        if (_button < 2)
+	        {
+	            _button++;
+	            Cursor.position += new Vector3(0f, -CursorMove);
+            }
+	    }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        MusikPlayer.Volume = MusikMax;
+        //Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Mute()
+    {
+        MuteText.text = MusikPlayer.MuteFlip() ? "Mute [v]" : "Mute [ ]";
+    }
+
+    public void Restart()
+    {
+
+    }
+}
